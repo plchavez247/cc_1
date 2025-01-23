cb3d3c78c266a6f49b63e46218d86584a962f7c9
Pablo Chavez <plchavez@usf.edu>
2025-01-23 12:29:32 -0500

    Initial commit- JavaScript Basics Challenge Setup

---
+++ cc_1.js
@@ -0,0 +1,23 @@
+let employeeName = `Ron Swanson`;
+const employeeID = 1234;
+var isActive = true;
+console.log(tyepof employeeName);
+console.log(employeeID);
+console.log(isAcitive);
+
+
+let productname = `Water bottle`;
+const productPrice = 2000;
+var isAvailable = true;
+
+let accountBalance = 120000
+accountBalance += 500
+console.log(accountBalance)
+
+let customerName = 'Bob Belcher';
+let welcomeMessage = "Welcome, " + customername + ". I'm glad you're here.";
+console.log(welcomeMessage);
+
+let isLoggedIn = true
+
+
