5438b1a5d633ff0841ce15d16ae8b0d94c405ea0
Pablo Chavez <plchavez@usf.edu>
2025-01-23 22:00:25 -0500

    cc_1.

--- cc_1.js
+++ cc_1.js
@@ -2,13 +2,16 @@ let employeeName = `Ron Swanson`;
 const employeeID = 1234;
 var isActive = true;
 console.log(tyepof employeeName);
-console.log(employeeID);
-console.log(isAcitive);
+console.log(typeof employeeID);
+console.log(typeof isAcitive);
 
 
 let productname = `Water bottle`;
 const productPrice = 2000;
 var isAvailable = true;
+console.log(typeof productname)
+console.log(typeof productprice)
+console.log(typeof isAvailable)
 
 let accountBalance = 120000
 accountBalance += 500
@@ -18,6 +21,9 @@ let customerName = 'Bob Belcher';
 let welcomeMessage = "Welcome, " + customername + ". I'm glad you're here.";
 console.log(welcomeMessage);
 
-let isLoggedIn = true
-
-
+let isLoggedIn = true;
+let hasAccess = false;
+console.log(isLoggedIn && hasAccess);
+console.log(isLoggedIn || hasAccess);
+console.log(!isLoggedIn)
+console.log(!hasAccess)
